545990f36f9657c40412b36c020c2796db9522ca
santipdmonte <santiagopedemonte02@gmail.com>
2025-02-10 17:26:34 -0300

    Add validations for user, employee, and service existence in booking creation

--- routes/bookings.py
+++ routes/bookings.py
@@ -10,7 +10,27 @@ router = APIRouter(prefix="/turnos", tags=["Turnos"])
 
 @router.post("/", response_model=TurnoResponse)
 async def crear_turno(turno: TurnoBase, db=Depends(get_db)):
+
     async with db.transaction():
+
+        # Verificar que el usuario exista
+        usuario = await db.fetchrow("SELECT id FROM usuarios WHERE id = $1", turno.usuario_id)
+        if not usuario:
+            raise HTTPException(status_code=404, detail="Usuario no encontrado")
+
+        # Verificar que el empleado exista
+        empleado = await db.fetchrow("SELECT id FROM empleados WHERE id = $1", turno.empleado_id)
+        if not empleado:
+            raise HTTPException(status_code=404, detail="Empleado no encontrado")
+        
+        # Verificar que el servicio exista
+        servicio = await db.fetchrow("SELECT id FROM servicios WHERE id = $1", turno.servicio_id)
+        if not servicio:
+            raise HTTPException(status_code=404, detail="Servicio no encontrado") 
+        
+        # Verifica que la fecha se mayor o igual a la actual
+        if turno.fecha < date.today():
+            raise HTTPException(status_code=400, detail="La fecha del turno no puede ser menor a la actual")
 
         # Verificar que el horario esté disponible
         disponible = await db.fetchval("""
@@ -26,28 +46,29 @@ async def crear_turno(turno: TurnoBase, db=Depends(get_db)):
         if not disponible:
             raise HTTPException(status_code=400, detail="El horario seleccionado no está disponible")
 
-        result  = await db.fetchrow("""
+        nuevo_turno   = await db.fetchrow("""
             INSERT INTO turnos (usuario_id, empleado_id, servicio_id, fecha, hora, estado)
             VALUES ($1, $2, $3, $4, $5, 'confirmado')
             RETURNING id, usuario_id, empleado_id, servicio_id, fecha, hora, estado;
         """, turno.usuario_id, turno.empleado_id, turno.servicio_id, turno.fecha, turno.hora)
         
-        if not result:
+        if not nuevo_turno :
             raise HTTPException(status_code=500, detail="Error al crear el turno")
         
-        result_update_horarios  = await db.fetchval("""
+        horario_actualizado  = await db.fetchval("""
             UPDATE horarios_disponibles 
             SET disponible = FALSE 
             WHERE 
                 fecha = $1 
                 AND hora = $2 
-                AND empleado_id = $3;
+                AND empleado_id = $3
+            RETURNING id;
         """, turno.fecha, turno.hora, turno.empleado_id)
         
-        if result_update_horarios == 0:
-            raise HTTPException(status_code=400, detail="El horario no está disponible")
+        if not horario_actualizado:
+            raise HTTPException(status_code=400, detail="No se pudo reservar el horario seleccionado")
         
-        return dict(result)
+        return dict(nuevo_turno)
     
 
 @router.get("/{turno_id}", response_model=TurnoResponse)
@@ -117,9 +138,11 @@ async def horarios_disponibles_by_date(fecha: date, db=Depends(get_db)):
 
 @router.put("/edit/{turno_id}", response_model=TurnoResponse)
 async def modificar_turno(turno_id: UUID, nuevo_turno: TurnoBase, db=Depends(get_db)):
+    
     async with db.transaction():
 
-        # Obtenemos el turno a editar
+
+        # Validamos que el turno a editar exista
         turno_anterior = await db.fetchrow("""
             SELECT empleado_id, fecha, hora 
             FROM turnos 
@@ -130,64 +153,36 @@ async def modificar_turno(turno_id: UUID, nuevo_turno: TurnoBase, db=Depends(get
         if not turno_anterior:
             raise HTTPException(status_code=404, detail="Turno no encontrado")
         
-        # Validamos si el nuevo horario esta disponible
-        disponible = await db.fetchval("""
-            SELECT EXISTS (
-                SELECT 1 FROM horarios_disponibles 
-                WHERE fecha = $1 
-                AND hora = $2 
-                AND empleado_id = $3 
-                AND disponible = TRUE
-            );
-        """, nuevo_turno.fecha, nuevo_turno.hora, nuevo_turno.empleado_id)
-        
-        if not disponible:
-            raise HTTPException(status_code=400, detail="El horario seleccionado no está disponible")
-        
-        # cancelamos el turno anterior
-        await db.execute("""
-            UPDATE turnos
-            SET estado = 'cancelado'
-            WHERE id = $1;
-        """,turno_id)
-        
-        # Actualizamos el horario del turno anterior como disponible
-        await db.execute("""
-            UPDATE horarios_disponibles 
-            SET disponible = TRUE 
-            WHERE fecha = $1 
-            AND hora = $2 
-            AND empleado_id = $3;
-        """, turno_anterior["fecha"], turno_anterior["hora"], turno_anterior["empleado_id"])
-
         # Creamos el nuevo turno
-        result  = await db.fetchrow("""
-            INSERT INTO turnos (usuario_id, empleado_id, servicio_id, fecha, hora, estado)
-            VALUES ($1, $2, $3, $4, $5, 'confirmado')
-            RETURNING id, usuario_id, empleado_id, servicio_id, fecha, hora, estado;
-        """, nuevo_turno.usuario_id, nuevo_turno.empleado_id, nuevo_turno.servicio_id, nuevo_turno.fecha, nuevo_turno.hora)
-        
-        # Actualizamos el horario del nuevo turno como no disponible
-        await db.execute("""
-            UPDATE horarios_disponibles 
-            SET disponible = FALSE 
-            WHERE fecha = $1 
-            AND hora = $2 
-            AND empleado_id = $3;
-        """, nuevo_turno.fecha, nuevo_turno.hora, nuevo_turno.empleado_id)
-    
-    return await obtener_turno(result["id"], db)
+        nuevo_turno = await crear_turno(nuevo_turno, db)
 
+        # cancelamos el turno anterior
+        await cancelar_turno(turno_id, db)
+    
+    return nuevo_turno
 
     
 
 @router.get("/user/{user_id}", response_model=list[TurnoResponse])
 async def obtener_turnos_by_user(user_id: UUID, db=Depends(get_db)):
-    query = "SELECT * FROM turnos WHERE usuario_id = $1 AND estado <> 'cancelado';"
-    turnos = await db.fetch(query, user_id)
-    if not turnos:
-        raise HTTPException(status_code=404, detail="No se encontraron turnos para el usuario")
-    return [dict(turno) for turno in turnos]
+    try:
+        # Validar que el usuario exista
+        usuario = await db.fetchrow("SELECT id FROM usuarios WHERE id = $1", user_id)
+        if not usuario:
+            raise HTTPException(status_code=404, detail="Usuario no encontrado")
+        
+        # Obtener los turnos del usuario
+        turnos = await db.fetch("""
+            SELECT * FROM turnos 
+                WHERE usuario_id = $1 
+                AND fecha >= CURRENT_DATE
+                AND estado <> 'cancelado';
+        """, user_id)
+
+        return [dict(turno) for turno in turnos] if turnos else []
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 
